83dbac88066eb8f3a71b14113e5577da29b2a742
vkjl <villem.laimre@gmail.com>
2016-12-12 01:50:55 +0200

    Suuremalt jaolt tehtud, peaks veel natuke grid abil paigutama ja seletavat teksti lisama.

--- cabc.py
+++ cabc.py
@@ -1,47 +1,349 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
+import winsound
 from random import randint
 
-# funktsioonid
 
-def alusta():
-    # uuendab akent ja paiguteb sinna kaks play nuppu
-
-    return
-
-
-# muutujad
-a = []
-b = []
-
-# tegelik algus
-root = Tk()
-root.title("Kõrva kontroll")
-
-frame1 = Frame(root)
-
-tervitus = ttk.Label(frame1, text="Tere tulemast!")
-tervitus.grid(column=0, row=0, padx=10, pady=10, sticky=(N, W))
-
-seletus = ttk.Label(frame1, text="""""")
-seletus.grid(column=0, row=2, padx=10, pady=10, sticky=(E, W ))
-
-start = ttk.Button(frame1, text="Alusta!", command=alusta())
-start.grid(column=0, row=4, padx=10, pady=10)
-
-frame1.columnconfigure(1, weight=1)
-frame1.rowconfigure(1, weight=1)
-
-frame2 = Frame(root)
-
-play_nupp_a = ttk.Button(frame2, text="Esimene heli", command=mangib_niisama())
-play_nupp_a.grid(column=0, row=0, padx=10, pady=10)
-
-play_nupp_b = ttk.Button(frame2, text="Teine heli", command=korda_meloodiat())
-play_nupp_b.grid(column=1, row=0, padx=10, pady=10)
-
-küsimus = tkk.Label(frame2, text="")
+c = '39188_jobro_piano-ff-041 (mp3cut.net).wav'
+cis = '39189_jobro_piano-ff-042 (mp3cut.net).wav'
+d = '39190_jobro_piano-ff-043 (mp3cut.net).wav'
+dis = '39191_jobro_piano-ff-044 (mp3cut.net).wav'
+e = '39193_jobro_piano-ff-045 (mp3cut.net).wav'
+f = '39194_jobro_piano-ff-046 (mp3cut.net).wav'
+fis = '39195_jobro_piano-ff-047 (mp3cut.net).wav'
+g = '39196_jobro_piano-ff-048 (mp3cut.net).wav'
+gis = '39197_jobro_piano-ff-049 (mp3cut.net).wav'
+a = '39198_jobro_piano-ff-050 (mp3cut.net).wav'
+ais = '39199_jobro_piano-ff-051 (mp3cut.net).wav'
+h = '39200_jobro_piano-ff-052 (mp3cut.net).wav'
+noodiJärjend = [c, cis, d, dis, e, f, fis, g, gis, a, ais, h]
 
 
+def genereeri_helisid(lst):
+    # teeb heli listi noodiJärjendist
 
-root.mainloop()
+    uus_list = []
+    for el in lst:
+        juhuslik = randint(0, 6)
+        a = lst[juhuslik]
+        uus_list.append(a)
+
+    return uus_list
+
+
+def mängib_niisama(lst):
+    for el in lst:
+        winsound.PlaySound(el, winsound.SND_FILENAME)
+
+
+def helide_muutja(lst):
+    # muuda ühte suvalist elementi
+
+    suvaline_element = randint(0, len(lst) - 1)
+    suvaline = lst[suvaline_element]
+    el_indeks_järjend = noodiJärjend.index(suvaline)
+
+    if randint(0, 1) == 1:
+        try:
+            lst[suvaline_element] = noodiJärjend[el_indeks_järjend + 1]
+        except:
+            lst[suvaline_element] = noodiJärjend[el_indeks_järjend - 1]
+    else:
+        try:
+            lst[suvaline_element] = noodiJärjend[el_indeks_järjend - 1]
+        except:
+            lst[suvaline_element] = noodiJärjend[el_indeks_järjend + 1]
+
+    return lst
+
+
+def korda_või_ära_korda_meloodiat(lst):
+
+    if randint(0, 1) == 1:
+        uus_list = lst
+
+    else:
+        uus_list = helide_muutja(lst)
+
+    return uus_list
+
+
+class Application(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.minsize(500, 300)
+        self.frames = {}
+
+        for F in (AlgusLeht, MuusikaKuulamine1, MuusikaKuulamine2, MuusikaKuulamine3, MuusikaKuulamine4,
+                  MuusikaKuulamine5, MuusikaKuulamine6, MuusikaKuulamine7, MuusikaKuulamine8,
+                  MuusikaKuulamine9, MuusikaKuulamine10, TulemusLeht):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(AlgusLeht)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class AlgusLeht(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Tervitus siia")
+        label.pack(pady=10, padx=10)
+
+        sissejuhTekst = ttk.Label(text="Seletav tekst siia")
+        sissejuhTekst.pack()
+
+        button1 = ttk.Button(self, text="Alusta",
+                             command=lambda: controller.show_frame(MuusikaKuulamine1))
+        button1.pack()
+
+
+class MuusikaKuulamine1(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 1/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = tk.Button(self, text="Esimene viis",
+                            command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = tk.Button(self, text="Teine viis",
+                            command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = tk.Button(self, text="Järgmine küsimus",
+                            command=lambda: controller.show_frame(MuusikaKuulamine2))
+        button1.pack()
+
+
+class MuusikaKuulamine2(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 2/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine3))
+        button1.pack()
+
+
+class MuusikaKuulamine3(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 3/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine4))
+        button1.pack()
+
+
+class MuusikaKuulamine4(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 4/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine5))
+        button1.pack()
+
+
+class MuusikaKuulamine5(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 5/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine6))
+        button1.pack()
+
+
+class MuusikaKuulamine6(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 6/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine7))
+        button1.pack()
+
+
+class MuusikaKuulamine7(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 7/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine8))
+        button1.pack()
+
+
+class MuusikaKuulamine8(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 8/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine9))
+        button1.pack()
+
+
+class MuusikaKuulamine9(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 9/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Järgmine küsimus",
+                             command=lambda: controller.show_frame(MuusikaKuulamine10))
+        button1.pack()
+
+
+class MuusikaKuulamine10(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Küsimus 10/10")
+        label.pack(pady=10, padx=10)
+
+        heliList = genereeri_helisid(noodiJärjend)
+        heliList2 = korda_või_ära_korda_meloodiat(heliList)
+
+        button2 = ttk.Button(self, text="Esimene viis",
+                             command=lambda: mängib_niisama(heliList))
+        button2.pack()
+
+        button3 = ttk.Button(self, text="Teine viis",
+                             command=lambda: mängib_niisama(heliList2))
+        button3.pack()
+
+        button1 = ttk.Button(self, text="Lõpeta küsitlus",
+                             command=lambda: controller.show_frame(TulemusLeht))
+        button1.pack()
+
+
+class TulemusLeht(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Tulemus")
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text="Tagasi algusesse",
+                             command=lambda: controller.show_frame(AlgusLeht))
+        button1.pack()
+
+
+app = Application()
+app.mainloop()
